63f459742cdf637cd74819ee9a62bfcb24fe6aad
lebrancconvas <lebrancconvas@gmail.com>
2022-10-30 08:23:46 +0700

    💡 refactor: add comments to explain code

--- mvc-ts/src/controllers/controller.ts
+++ mvc-ts/src/controllers/controller.ts
@@ -1,5 +1,33 @@
-class Controller {
+import fs from 'fs';
 
+class Controller {
+	// File Reading Function. 
+	readFile(path: string): string {
+		const output = fs.readFileSync(path).toString('utf-8');
+		return output;
+	}
+
+	// Get Data from Data File. 
+	getDataFromFile(fileExtension: string, fileName: string) {
+		let result;
+		switch(fileExtension) {
+			case 'json':
+				result = this.readFile(`./models/data/${fileExtension}/${fileName}.json`);
+				break;
+			case 'text':
+				result = this.readFile(`./models/data/${fileExtension}/${fileName}.txt`);
+				break;
+			case 'xml':
+				result = this.readFile(`./models/data/${fileExtension}/${fileName}.xml`);
+				break;
+			case 'csv':
+				result = this.readFile(`./models/data/${fileExtension}/${fileName}.csv`);
+				break;
+		}
+		return result;
+	}
+
+	// Working Function (Function that you want to do for your work.) 
 }
 
 export default Controller;
